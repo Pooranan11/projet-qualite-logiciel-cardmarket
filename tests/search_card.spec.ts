be757dc06ef3f71500c3f4910c927bf7685d1939
Pooranan Vytheswaran <vytheswaran@et.esiea.fr>
2025-12-28 14:07:03 +0100

    Test E2E Playwright ajout de pause

--- tests/search_card.spec.ts
+++ tests/search_card.spec.ts
@@ -8,11 +8,15 @@ test('Recherche de carte Pokémon sur CardMarket (scénario BDD)', async ({ page
 
   // Given
   await homePage.open();
+  await page.pause();
   await homePage.selectPokemonGame();
-
+  await page.pause();
+  
   // When
-  await homePage.searchCard('Black Lotus');
-
+  await homePage.searchCard('Palkia Originel VSTAR GG67'); // Je cherche réelement cette carte :(
+  await page.pause();
+  
   // Then
   await resultsPage.resultsAreDisplayed();
+  await page.pause();
 });
