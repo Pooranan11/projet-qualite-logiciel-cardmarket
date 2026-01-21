335cfae982a857525794e024b21f7b323d7367f1
Pooranan Vytheswaran <vytheswaran@et.esiea.fr>
2026-01-21 10:06:02 +0100

    modif du headless pour GitHub Action

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     /* Anti-Cloudflare settings */
-    headless: false, // Affiche la fenêtre pour éviter le ban Cloudflare
+    headless: !!process.env.CI, // Affiche la fenêtre pour éviter le ban Cloudflare
     viewport: { width: 1280, height: 720 },
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
   },
